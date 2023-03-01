72e6324699f6c87135e4cd92516a3d9a55a7330a
kaesik <108717713+kaesik@users.noreply.github.com>
2023-03-01 19:20:39 +0100

    completed day 6

---
+++ Day 6/6.0.py
@@ -0,0 +1,16 @@
+#DEFINING FUNCTION
+def my_function():
+    print("Hello")
+    print("Bye")
+
+#CALLING FUNCTION
+my_function()
+
+#INDENTATION
+def my_function():
+    print("Hello")
+#to ta przerwa przed printem, 4 spacje = 1 tab
+
+#WHILE LOOP
+while True:
+    print("Hello")
