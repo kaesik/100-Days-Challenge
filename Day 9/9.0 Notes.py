38da30897635015f0cc9f11c63b000804b2a4ccc
kaesik <108717713+kaesik@users.noreply.github.com>
2023-03-06 22:22:10 +0100

    updating notes of "nesting dictionary"

--- Day 9/9.0 Notes.py
+++ Day 9/9.0 Notes.py
@@ -32,3 +32,35 @@ print(empty_dict)
 for thing in empty_dict:
     print(thing)  # ONLY KEYS
     print(empty_dict[thing])  # KEYS AND VALUES
+
+#NESTING
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+travel_log_2 = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+        },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+         },
+}
+travel_log_3 = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5,
+     },
+]
