bf77dc338d02be4298b1128bd0dfc1421f1e5951
kaesik <108717713+kaesik@users.noreply.github.com>
2023-03-07 19:43:47 +0100

    all functions from "Calculator"

---
+++ Day 10/Calculator_functions.py
@@ -0,0 +1,36 @@
+
+
+
+def add(num_a, num_b):
+    """Function adds num_a to num_b"""
+    return num_a + num_b
+
+
+def subtract(num_a, num_b):
+    """Function subtracts num_b from num_a"""
+    return num_a - num_b
+
+
+def multiply(num_a, num_b):
+    """Function multiply num_a by num_b"""
+    return num_a * num_b
+
+
+def divide(num_a, num_b):
+    """Function divide num_a by num_b"""
+    return num_a / num_b
+
+
+def calculation(num_a, num_b, operation):
+    function = operations[operation]
+    answer = function(num_a, num_b)
+    return answer
+
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
