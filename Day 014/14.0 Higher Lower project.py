8bcd12ab6515baae3c23a5549b82ff9a7d1ced8f
kaesik <108717713+kaesik@users.noreply.github.com>
2023-03-12 11:44:07 +0100

    rewrite placeholder text

--- Day 014/14.0 Higher Lower project.py
+++ Day 014/14.0 Higher Lower project.py
@@ -2,7 +2,7 @@ from HigherLower_arts import *
 from HigherLower_functions import *
 
 print(logo)
-print("placeholder: welcome, play")
+print("Welcome in Higher-Lower Game!\nYou need to guess who has more followers on Instagram!")
 while True:
     score = 0
     person_a = fun_random_person()
@@ -17,22 +17,22 @@ while True:
         print(option_B)
         print(f"{fun_person_data(person_b)[0]}, {fun_person_data(person_b)[2]} from {fun_person_data(person_b)[3]}.")
         print("──────────────────────────────────────────")
-        guess = input("placeholder: write 'A' or 'B': ").lower()
+        guess = input("Who has more followers on Instagram? Type 'A' or 'B': ").lower()
         if guess == "a" and fun_win_round(fun_person_data(person_a)[1], fun_person_data(person_b)[1])\
                 or guess == "b" and fun_win_round(fun_person_data(person_b)[1], fun_person_data(person_a)[1]):
             score += 1
-            print(f"placeholder: gz, your score: {score}")
+            print(f"Good answer! Your score: {score}")
             if guess == "a":
                 person_b = person_a
             elif guess == "b":
                 person_a = person_b
         elif guess == "a" and fun_win_round(fun_person_data(person_b)[1], fun_person_data(person_a)[1])\
                 or guess == "b" and fun_win_round(fun_person_data(person_a)[1], fun_person_data(person_b)[1]):
-            print(f"placeholder: you lose, your score: {score}")
+            print(f"Wrong answer! Your final score: {score}")
             break
         else:
             print("Incorrect decision. Please repeat.\n")
             continue
-    game = input("placeholder: would you like play again? y/n\n").lower()
+    game = input("Would you like to play again? y/n\n").lower()
     if game != 'y':
         break
