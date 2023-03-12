89ff35be616d43e653424018d2b0a84026005944
kaesik <108717713+kaesik@users.noreply.github.com>
2023-03-12 18:34:01 +0100

    variables for Coffee Machine project

---
+++ Day 015/CoffeeMachine_variables.py
@@ -0,0 +1,8 @@
+from CoffeeMachine_data import *
+from CoffeeMachine_functions import *
+
+water = list(list(resources.items())[0])
+milk = list(list(resources.items())[1])
+coffee = list(list(resources.items())[2])
+money = 0
+
