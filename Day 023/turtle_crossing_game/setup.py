5825ae34a43eaf58afaad58281caae086448812c
kaesik <108717713+kaesik@users.noreply.github.com>
2023-04-05 21:13:10 +0200

    setup and background for Road Crossing Game

---
+++ Day 023/turtle_crossing_game/setup.py
@@ -0,0 +1,14 @@
+# PLAYER
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+# CAR MANAGER
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 5
+
+# SCOREBOARD
+FONT = ("Courier", 24, "normal")
+FONT_GAMEOVER = ("Courier", 36, "bold")
+ALIGN = "center"
