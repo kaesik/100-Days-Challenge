d06bb4a47a11d1f3220588222b572b544d616c2d
kaesik <108717713+kaesik@users.noreply.github.com>
2023-04-05 21:13:47 +0200

    Road Crossing Game

---
+++ Day 023/turtle_crossing_game/main.py
@@ -0,0 +1,43 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+from setup import *
+
+# SCREEN SETUP
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+# OBJECTS
+player = Player()
+manager = CarManager()
+score = Scoreboard()
+
+# CONTROLS
+screen.listen()
+screen.onkeypress(player.up, "Up")
+
+# GAME
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    manager.create_car()
+    manager.move()
+    screen.update()
+
+# DETECTS NEXT LEVEL
+    if player.ycor() >= 260:
+        score.add_score()
+        STARTING_MOVE_DISTANCE += MOVE_INCREMENT
+        player.goto(STARTING_POSITION)
+
+# DETECT COLLISION WITH CARS
+    for car in manager.all_cars:
+        if car.distance(player) < 30:
+            player.died()
+            score.game_over()
+            game_is_on = False
+
+screen.exitonclick()
