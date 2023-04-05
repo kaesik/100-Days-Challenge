ce44455d08c0b427c250dd6c8f7dd02affe40838
kaesik <108717713+kaesik@users.noreply.github.com>
2023-04-05 21:13:37 +0200

    car manager class for Road Crossing Game

---
+++ Day 023/turtle_crossing_game/car_manager.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+from random import *
+from setup import *
+
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = randint(1, 6)
+        if random_chance == 6:
+            new_car = Turtle("square")
+            new_car.color(choice(COLORS))
+            new_car.pu()
+            new_car.seth(180)
+            new_car.shapesize(1, 2)
+            new_car.goto(300, randrange(-260, 260, 40))
+            self.all_cars.append(new_car)
+
+    def move(self):
+        for car in self.all_cars:
+            car.fd(5)
