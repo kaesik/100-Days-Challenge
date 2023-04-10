72d5c9b0fc7d5a822db715d524115beeee53c7ca
kaesik <108717713+kaesik@users.noreply.github.com>
2023-04-10 09:33:54 +0200

    Introduction to Pandas Module

--- Day 025/25.0 Notes.py
+++ Day 025/25.0 Notes.py
@@ -13,26 +13,55 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(data["temp"])
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# print(sum(temp_list)/len(temp_list))
+# print(data["temp"].mean())
+#
+# print(data["temp"].max())
+#
+# print(data.temp)
+#
+# print(data[data.day == "Monday"])
+#
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# monday_temp_f = monday.temp * 9/5 + 32
+# print(monday_temp_f)
+#
+# random_dict = {
+#     "students": ["Kamil", "Olaf", "Ada"],
+#     "scores": [22, 42, 45]
+# }
+# data = pandas.DataFrame(random_dict)
+# data.to_csv("new_data.csv")
 
-data_dict = data.to_dict()
-print(data_dict)
+grey = 0
+red = 0
+black = 0
 
-temp_list = data["temp"].to_list()
-print(temp_list)
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-print(sum(temp_list)/len(temp_list))
-print(data["temp"].mean())
+for color in data["Primary Fur Color"]:
+    if color == "Gray":
+        grey += 1
+    elif color == "Cinnamon":
+        red += 1
+    else:
+        black += 1
 
-print(data["temp"].max())
+squirrel_color = {
+    "Fur Color": ["grey", "red", "black"],
+    "Count": [grey, red, black]
+}
 
-print(data.temp)
-
-print(data[data.day == "Monday"])
-
-print(data[data.temp == data.temp.max()])
-
-monday = data[data.day == "Monday"]
-monday_temp_f = monday.temp * 9/5 + 32
-print(monday_temp_f)
+data = pandas.DataFrame(squirrel_color)
+data.to_csv("squirrel_color_data.csv")
