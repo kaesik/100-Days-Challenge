bd75787d8becf011fb9dd490a32e3aa5fdf4a426
kaesik <108717713+kaesik@users.noreply.github.com>
2023-04-16 22:19:12 +0200

    Day 28 of challenge

---
+++ Days 022 - 030/Day 028/pomodoro_app/main.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+FONT_TIMER = (FONT_NAME, 35, "bold")
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro 🍅")
+window.config(pady=50, padx=100,  bg=YELLOW)
+
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tk.PhotoImage(file="./tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=FONT_TIMER)
+canvas.pack()
+
+window.mainloop()
