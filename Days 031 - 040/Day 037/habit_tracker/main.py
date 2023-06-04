4a82e7beb730212c5d79bdc7d93a7d76542c315d
kaesik <108717713+kaesik@users.noreply.github.com>
2023-06-04 22:16:54 +0200

    Day 37 of challenge DONE

---
+++ Days 031 - 040/Day 037/habit_tracker/main.py
@@ -0,0 +1,46 @@
+import requests as rq
+from datetime import datetime
+
+USERNAME = "kaestoja"
+TOKEN = "q1w2e3r4t5qqq"
+GRAPH_ID = "graph2"
+
+today = datetime.now().strftime("%Y%m%d")
+
+PIXELA_ENDPOINT_USER = "https://pixe.la/v1/users"
+PIXELA_ENDPOINT_GRAPH = f"{PIXELA_ENDPOINT_USER}/{USERNAME}/graphs"
+PIXELA_ENDPOINT_PIXEL = f"{PIXELA_ENDPOINT_GRAPH}/{GRAPH_ID}"
+PIXELA_ENDPOINT_CHANGE_DELETE = f"{PIXELA_ENDPOINT_PIXEL}/{today}"
+
+user_params = {
+        "token": TOKEN,
+        "username": USERNAME,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
+}
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "kuro"
+}
+pixel_params = {
+    "date": today,
+    "quantity": "15.48",
+}
+change_params = {
+    "quantity": "18.57",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+
+# response = rq.post(url=PIXELA_ENDPOINT_USER, json=user_params)
+# response = rq.post(url=PIXELA_ENDPOINT_GRAPH, json=graph_params, headers=headers)
+# response = rq.post(url=PIXELA_ENDPOINT_PIXEL, json=pixel_params, headers=headers)
+# response = rq.put(url=PIXELA_ENDPOINT_CHANGE_DELETE, json=change_params, headers=headers)
+response = rq.delete(url=PIXELA_ENDPOINT_CHANGE_DELETE, headers=headers)
+
+print(response.text)
