928c7ca3c885623ca80011018308ccc6879a7791
kaesik <108717713+kaesik@users.noreply.github.com>
2023-07-19 20:53:04 +0200

    Day 57 of the 100 days challenge DONE

--- 3_Intermediate+/Day 57/blog_templating/main.py
+++ 3_Intermediate+/Day 57/blog_templating/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import requests as rq
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = rq.get(blog_url).json()
+    return render_template("index.html", posts=response)
+
+@app.route('/post/<num>')
+def get_post(num):
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = rq.get(blog_url).json()
+    return render_template("post.html", post=response[int(num)-1])
 
 if __name__ == "__main__":
     app.run(debug=True)
