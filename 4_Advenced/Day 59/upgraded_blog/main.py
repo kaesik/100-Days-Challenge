df9f5064c1c152eb3b4fb95f1bd6ae5de6297654
kaesik <108717713+kaesik@users.noreply.github.com>
2023-07-30 17:40:25 +0200

    Day 59 of the 100 days challenge DONE

--- 4_Advenced/Day 59/upgraded_blog/main.py
+++ 4_Advenced/Day 59/upgraded_blog/main.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+import requests as rq
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")    # render_template() will look for the file in the templates folder
+blog_url = "https://api.npoint.io/5880f2a78644ec00a63b"
+response = rq.get(blog_url).json()
 
+@app.route('/')
+def main_page():
+    return render_template("index.html", posts=response)
+
+@app.route('/about')
+def about_page():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
+
+@app.route('/blog/<num>')
+def blog_page(num):
+    post = response[int(num) - 1]
+    return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
