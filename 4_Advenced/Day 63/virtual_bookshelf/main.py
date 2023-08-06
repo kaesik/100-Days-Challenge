ac2e7992d4fd3df83eff75c0776e5889fb0145f0
kaesik <108717713+kaesik@users.noreply.github.com>
2023-08-06 22:39:43 +0200

    Day 63 of the 100 days challenge

--- 4_Advenced/Day 63/virtual_bookshelf/main.py
+++ 4_Advenced/Day 63/virtual_bookshelf/main.py
@@ -1,27 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///virtual_bookshelf.db"
+db.init_app(app)
 
-all_books = []
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-
-    return render_template("index.html", books=all_books)
+    with app.app_context():
+        all_books = db.session.query(Book).all()
+        print(all_books)
+        return render_template("index.html", books=all_books)
+    return render_template("index.html")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
-        print(all_books)
-        return redirect(url_for("home"))
+        with app.app_context():
+            new_book = Book(
+                title=request.form["title"],
+                author=request.form["author"],
+                rating=request.form["rating"]
+            )
+            print(new_book)
+            db.session.add(new_book)
+            db.session.commit()
+            return redirect(url_for("home"))
     return render_template("add.html")
 
 
