421f7e59bb95c0ac7830c2159a763bbf65bddaef
kaesik <108717713+kaesik@users.noreply.github.com>
2023-08-07 20:39:55 +0200

    Day 63 of the 100 days challenge dONE

--- 4_Advenced/Day 63/virtual_bookshelf/main.py
+++ 4_Advenced/Day 63/virtual_bookshelf/main.py
@@ -21,7 +21,6 @@ with app.app_context():
 def home():
     with app.app_context():
         all_books = db.session.query(Book).all()
-        print(all_books)
         return render_template("index.html", books=all_books)
     return render_template("index.html")
 
@@ -33,13 +32,33 @@ def add():
             new_book = Book(
                 title=request.form["title"],
                 author=request.form["author"],
-                rating=request.form["rating"]
+                rating=request.form["rating"].replace(",", ".")
             )
             print(new_book)
             db.session.add(new_book)
             db.session.commit()
             return redirect(url_for("home"))
     return render_template("add.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"].replace(",", ".")
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"].replace(",", ".")
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
